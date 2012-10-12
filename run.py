ce957bf22751f9eaadb7487b92381f240eb26250
Dilip Joseph <dilipjoseph@Dilip-Josephs-MacBook-Pro.local>
2012-10-11 21:45:58 -0700

    Added web.py webserver serving index.html

--- run.py
+++ run.py
@@ -1,6 +1,27 @@
 import sys
+import web
 
 from Analyzer import Analyzer
+
+        
+
+class DefaultHandler:        
+    def GET(self, name):
+        raise web.seeother('/static/index.html')
+
+def startWebServer():
+    URLS = (
+        '/(.*)', 'DefaultHandler'
+    )
+
+    app = web.application(URLS, globals())
+    # If command line arguments are specified, web.py assumes that it 
+    # is the host and port.  Wierd!!
+    # So need to jump through hoops to specify the port and use
+    # command line arguments for something else.
+    del sys.argv[:] # This is the python way to clear a list
+    sys.argv.insert(0, "8080") 
+    app.run()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -12,4 +33,9 @@ if __name__ == "__main__":
     analyzer.processFile(logFileName)
 
     print analyzer
+    
+    print "Starting webserver"
+
+    startWebServer()
+
 
